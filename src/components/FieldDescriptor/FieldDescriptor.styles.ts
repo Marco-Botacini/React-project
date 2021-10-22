b17e871a25e32997f24fb6a91fc89f23d8d43ffd
Marco Botacini <90485215+Marco-Botacini@users.noreply.github.com>
2021-10-22 12:17:49 -0300

    added FieldDescriptor component

---
+++ src/components/FieldDescriptor/FieldDescriptor.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  color: #274060;
+`
+
+export const Field = styled.span`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  color: #274060;
+`
+
+export const Value = styled.span`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  color: #274060;
+`
